42bb3e5c7a142f1b4da8ae5d04df6829d6b05626
Niklas265 <59866153+Niklas265@users.noreply.github.com>
2023-08-22 13:46:27 +0200

    started impacket smb

--- smbinterestingfilefinder.py
+++ smbinterestingfilefinder.py
@@ -1,11 +1,12 @@
 import ldap
 from nslookup import Nslookup
-import smbclient
+from impacket.smbconnection import SMBConnection
+from impacket import smb
 
-basedn="xxxxx"
+basedn="xxxxxx"
 dc_ip = "xxxx"
-username = "xxxxx"
-domain = "xxxxxx"
+username = "xxx"
+domain = "xxxxx"
 password = "xxxxxx"
 
 def connect_ldap(ldapServer: str, user: str,password: str):
@@ -42,6 +43,43 @@ def resolve_hostname(dns_server: str,host: str, domain: str):
     else:
         return None
 
+def connect_smb(ip:str):
+    try:
+        smbClient = SMBConnection(ip,ip)
+        smbClient.login(username,password,domain,'','')
+        return smbClient
+    except:
+        return None
+
+def get_smb_share_list(con):
+    ret = []
+    try:
+        shares = con.listShares()
+        for i in shares:
+            ret.append(i["shi1_netname"].rstrip('\x00'))
+    except:
+        pass
+    return ret
+
+
+def recurse_share(con,sharename):
+    try:
+        print(con.listPath(sharename,'\\'))
+    except smb.SessionError as e:
+        print(e)
+
+def traverse_shares(share_list,con):
+    print(share_list)
+    # Iterate over all shares but skip uninteresting shares like admin$, ipc$ and sysvol
+    for share in share_list:
+        if share.lower() != 'admin$' and share.lower() != 'sysvol' and share.lower() != 'ipc$':
+            try:
+                print(share)
+                share_con = con.connectTree(share) 
+                recurse_share(share_con,share)
+            except:
+                print("Error")
+
 
 def main():
     connection = connect_ldap(f"ldap://{dc_ip}",f"{username}@{domain}",password)
@@ -55,6 +93,10 @@ def main():
         ip = resolve_hostname(dc_ip,host,domain)
         if ip != None:
             print(ip)
+            connection = connect_smb(ip)
+            if connection != None:
+                share_list = get_smb_share_list(connection)
+                traverse_shares(share_list,connection)
 
 if __name__ == "__main__":
     main()
