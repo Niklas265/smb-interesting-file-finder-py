6354b40ff130f633f08a472f908f633b8effa415
Niklas265 <59866153+Niklas265@users.noreply.github.com>
2023-09-01 11:15:56 +0200

    added argument support

--- smbinterestingfilefinder.py
+++ smbinterestingfilefinder.py
@@ -1,3 +1,4 @@
+import os
 import ldap
 from nslookup import Nslookup
 from impacket.smbconnection import SMBConnection
@@ -5,14 +6,31 @@ from impacket import smb
 from impacket.smbconnection import SMB2_DIALECT_002
 from impacket.examples.smbclient import MiniImpacketShell
 import ntpath
+import argparse
 
-basedn="xxxxx"
-dc_ip = "xxxxx"
-username = "xxxxx"
-domain = "xxxxx"
-password = "xxxxxx"
+def parseDN(domainname:str):
+    dn = ""
+    first = True
+    for i in domainname.split("."):
+        if first is False:
+            dn = dn + ","
+        dn = dn + f"DC={i.upper()}"
+        first = False
+    return dn
 
-searchList = ["password","admin","config"]
+def parseSearchList(path:str):
+    try:
+        search_terms = []
+        if os.path.exists(path) and os.path.isfile(path):
+            file = open(path,"r")
+            if file.readable():
+                lines = file.readlines()
+                for line in lines:
+                    search_terms.append(line[:-1])
+                return search_terms
+        return []
+    except:
+        return []
 
 def connect_ldap(ldapServer: str, user: str,password: str):
     ldap_con = ldap.initialize(ldapServer)
@@ -48,7 +66,7 @@ def resolve_hostname(dns_server: str,host: str, domain: str):
     else:
         return None
 
-def connect_smb(ip:str):
+def connect_smb(ip:str,username:str,password:str,domain:str):
     try:
         smbClient = SMBConnection(ip,ip,sess_port=445,preferredDialect=None)
         print(smbClient.getDialect())
@@ -68,14 +86,13 @@ def get_smb_share_list(con):
         pass
     return ret
 
-def eval_filename(filename):
-    for i in searchList:
+def eval_filename(filename,keywords):
+    for i in keywords:
         if i in filename:
-            print(filename)
             return True
     return False
 
-def recurse_share(con,sharename,directory):
+def recurse_share(con,sharename,directory,keywords):
     try:
         files = con.listPath(sharename,directory)
         for i in files:
@@ -83,16 +100,16 @@ def recurse_share(con,sharename,directory):
                 if i.is_directory() and i.get_shortname() != "." and i.get_shortname() != "..":
                     #print(i.get_shortname())
                     new_dir = directory[:-1]
-                    recurse_share(con,sharename,directory[:-1]+i.get_shortname()+"/*")
+                    recurse_share(con,sharename,directory[:-1]+i.get_shortname()+"/*",keywords)
                 elif not i.is_directory():
-                    if eval_filename(i.get_shortname()):
+                    if eval_filename(i.get_shortname(),keywords):
                         print(f"//{con.getRemoteHost()}/{sharename}/{directory[:-1]}{i.get_shortname()}")
             except:
                 pass
     except smb.SessionError as e:
         print(e)
 
-def traverse_shares(share_list,con):
+def traverse_shares(share_list,con,keywords):
     print(share_list)
     # Iterate over all shares but skip uninteresting shares like admin$, ipc$ and sysvol
     for share in share_list:
@@ -105,16 +122,42 @@ def traverse_shares(share_list,con):
                 #continue
                 share_con = con.connectTree(share)
                 print(share_con)
-                recurse_share(con,str(share),"*")
+                recurse_share(con,str(share),"*",keywords)
             except:
                 print("Error")
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Tool to find interesting files, that are accessible on shares inside a domain")
+    parser.add_argument('--dc-ip', action='store', type=str, help='IP of Domain Controller', required=True)
+    parser.add_argument('-u',"--username", action='store', type=str, help='Username', required=True)
+    parser.add_argument('-p',"--password", action='store', type=str, help='Password', required=True)
+    parser.add_argument('-d',"--domain", action='store', type=str, help='FQDN of the domain', required=True)
+    parser.add_argument('-s',"--search", action='store', type=str, help='Path to file with searchterms', required=True)
+
+    parser.add_argument('-f','--filter', action='store', type=str, help="Regex LDAP-Filter for specific computer objects, such as *dc*", required=False)
+    
+    arguments = parser.parse_args()
+    
+    dc_ip = arguments.dc_ip
+    username = arguments.username
+    password = arguments.password
+    domain = arguments.domain
+    basedn = parseDN(domain)
+    keywords = parseSearchList(arguments.search)
+    if len(keywords) == 0:
+        print("[-] Empty list of searchterms or unable to read searchterm file")
+        return
+
     connection = connect_ldap(f"ldap://{dc_ip}",f"{username}@{domain}",password)
     if not connection:
         return
-    results = ldap_query(connection, basedn,"(objectCategory=computer)")
+
+    ldap_filter = "(objectCategory=computer)"
+    if arguments.filter:
+        ldap_filter = f"(&{ldap_filter}(name={arguments.filter}))"
+    
+    results = ldap_query(connection, basedn,ldap_filter)
     host_list = parse_computers(results)
     print(f"[+] Found {len(host_list)} computers!")
     for host in host_list:
@@ -122,10 +165,10 @@ def main():
         ip = resolve_hostname(dc_ip,host,domain)
         if ip != None:
             print(ip)
-            connection = connect_smb(ip)
+            connection = connect_smb(ip,username,password,domain)
             if connection != None:
                 share_list = get_smb_share_list(connection)
-                traverse_shares(share_list,connection)
+                traverse_shares(share_list,connection,keywords)
 
 if __name__ == "__main__":
     main()
